040e4c0a7cf6f6cbb63cab3bb8bf9aa748aacc15
(Student) - Eric O Toole <C00203756@itcarlow.ie>
2017-03-09 09:20:17 +0000

    Thor library implementation

--- JointProject_TeamE/JointProject_TeamE/main.cpp
+++ JointProject_TeamE/JointProject_TeamE/main.cpp
@@ -4,26 +4,28 @@
  #pragma comment(lib, "sfml-system-d.lib")
  #pragma comment(lib, "sfml-window-d.lib")
  #pragma comment(lib, "sfml-network-d.lib")
-#pragma comment(lib, "libyaml-cppmdd.lib") // YAML
+ #pragma comment(lib, "libyaml-cppmdd.lib") // YAML
+ #pragma comment(lib,"thor-d.lib")			//Thor library
  #else
  #pragma comment(lib, "sfml-graphics.lib")
  #pragma comment(lib, "sfml-audio.lib")
  #pragma comment(lib, "sfml-system.lib")
  #pragma comment(lib, "sfml-window.lib")
  #pragma comment(lib, "sfml-network.lib")
-#pragma comment(lib, "libyaml-cppmd.lib") // YAML
+ #pragma comment(lib, "libyaml-cppmd.lib") // YAML
+ #pragma comment(lib,"thor.lib")		   //Thor library
  #endif
  #pragma comment(lib, "opengl32.lib")
  #pragma comment(lib, "glu32.lib")
 
 #include "Game.h"
 
-
 int main()
 {
 	//std::cout << "All hail the PeterLowe organization" << std::endl;
 	Game game;
 	game.run();
+	
 
 	return 0;
 }
