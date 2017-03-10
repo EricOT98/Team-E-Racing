73b3cf4bd24ec8d684de2bd30374e4e0b54ee3d0
DarrenSweeney <darrensweeneydev@gmail.com>
2017-03-10 12:01:29 +0000

    Implemented the basis of the Main Menu containing the GUI widgets

---
+++ JointProject_TeamE/JointProject_TeamE/Screens/MainMenu.h
@@ -0,0 +1,38 @@
+/// <summary>
+/// @author Darren Sweeney
+/// @version 1.0
+/// </summary>
+
+#ifndef MAIN_MENU_H
+#define MAIN_MENU_H
+
+#include "Screen.h"
+#include "..\GUI\Gui.h"
+#include "..\GUI\Button.h"
+#include "..\Input\XboxController.h"
+
+/// The Main Menu screen that the player navigates in to other screens.
+/// 
+/// The Main Menu contains button objects that will be updated in the Gui screen
+/// manager and allows the player to navigate the menu.
+class MainMenu : Screen
+{
+public:
+	MainMenu();
+	~MainMenu();
+
+	void update();
+	void render(sf::RenderWindow &window);  
+	void processInput(XboxController &controller);
+
+private:
+	Gui m_screenGUI;
+	Button *m_raceButton;
+	Button *m_upgradesButton;
+	Button *m_trophiesButton;
+	Button *m_optionsButton;
+	Button *m_creditsButton;
+	Button *m_exitButton;
+};
+
+#endif 
